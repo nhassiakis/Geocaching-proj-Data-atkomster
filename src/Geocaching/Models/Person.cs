a0d0ed610b8e57932b247c0dcf5f72e8fe65267f
LAPTOP-HLTN7FRC\Alexander <alexander.glandberger@outlook.com>
2019-03-19 13:53:24 +0100

    data annotations

--- src/Geocaching/Models/Person.cs
+++ src/Geocaching/Models/Person.cs
@@ -11,13 +11,19 @@ namespace Geocaching.Models
     {
         [Key]
         public int ID { get; set; }
+        [MaxLength(50)]
         public string FirstName { get; set; }
+        [MaxLength(50)]
         public string LastName { get; set; }
         public float Latitude { get; set; }
         public float Longitude { get; set; }
+        [MaxLength(50)]
         public string Country { get; set; }
+        [MaxLength(50)]
         public string City { get; set; }
+        [MaxLength(50)]
         public string StreetName { get; set; }
+        [MaxLength(50)]
         public Int16 StreetNumber { get; set; }
         public IList<FoundGeocache> FoundGeocaches { get; set; }
 
